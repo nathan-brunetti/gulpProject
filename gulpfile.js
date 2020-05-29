69439f2f14391d13d52435aae94274d3a6f9b8ac
Nathan Brunetti <njbrunetti12@gmail.com>
2020-05-29 15:48:33 -0600

    Here is another update

--- gulpfile.js
+++ gulpfile.js
@@ -35,7 +35,17 @@ function clear() {
 function img() {
 	return gulp
 	.src(paths.src.imgs)
-		.pipe(imagemin())
+		.pipe(imagemin([
+			imagemin.gifsicle({interlaced: true}),
+			imagemin.mozjpeg({quality: 75, progressive: true}),
+			imagemin.optipng({optimizationLevel: 5}),
+			imagemin.svgo({
+				plugins: [
+					{removeViewBox: true},
+					{cleanupIDs: false}
+				]
+			})
+		]))
 		.pipe(gulp.dest(paths.dist.imgs));
 }
 
